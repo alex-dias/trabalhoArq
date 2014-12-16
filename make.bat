7aaf8b685fc96d43b829ed32e606c4ac98b5f3f6
Leticia Domingues <leticia.domingues@hotmail.com>
2014-12-16 01:33:46 -0200

    Modificação no make.bat
    Modificação para executar o filtro na versão sequencial e paralela

--- make.bat
+++ make.bat
@@ -1,3 +1,5 @@
 cl GrayscaleFilter.c
+cl GrayscaleFilterSIMD.c
 
-for /f %%f in ('dir /b in\') do GrayscaleFilter in\%%f  out\%%f
+for /f %%f in ('dir /b in\') do GrayscaleFilter in\%%f out\Serial\%%f
+for /f %%f in ('dir /b in\') do GrayscaleFilterSIMD in\%%f out\SIMD\%%f
